3d5aa330df7dba0292b4bb8836222ed1d21dbd59
yassan-s <yasuro.2103@gmail.com>
2024-08-10 00:38:35 +0900

    ポケモン図鑑の実装 代1世代の初期表示

--- lib/model/call_pokemon_api.dart
+++ lib/model/call_pokemon_api.dart
@@ -2,16 +2,35 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+/// pokeAPIを呼び出すためのクラス
 class CallPokeApi {
 
   /// ポケモンの取得
-  Future<String> getPokemon({required String id}) async {
+  Future<Map> getPokemon({required String id}) async {
     var url = Uri.https("pokeapi.co", "/api/v2/pokemon/${id}/");
     var response = await http.get(url);
-
     var responseJson = jsonDecode(response.body) as Map<String, dynamic>;
-    var resultApiText = responseJson["sprites"]["front_default"];
-
-    return resultApiText;
+    return responseJson;
   }
+
+  /// 全世代の世代取得APIのURL取得
+  Future<Map> getALLGenerationApiUrl() async {
+    // 全世代のAPIを呼ぶために、APIのURLを取得
+    var url = Uri.https("pokeapi.co", "/api/v2/generation/");
+    var response = await http.get(url);
+    var responseJson = jsonDecode(response.body) as Map<String, dynamic>;
+    return responseJson;
+  }
+
+  /// 世代データ取得API呼び出し
+  ///
+  /// https://pokeapi.co/docs/v2#games-section
+  Future<Map> getPokemonFromGeneration(String geneInt) async {
+    // 全世代のAPIを呼ぶために、APIのURLを取得
+    var url = Uri.https("pokeapi.co", "/api/v2/generation/${geneInt}");
+    var response = await http.get(url);
+    var responseJson = jsonDecode(response.body) as Map<String, dynamic>;
+    return responseJson;
+  }
+
 }
--- lib/page/pokemon_picturebook/top.dart
+++ lib/page/pokemon_picturebook/top.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_try_new_feature/model/call_pokemon_api.dart';
 
@@ -11,73 +13,116 @@ class PokemonPictureBookTop extends StatefulWidget {
 
 class _PokemonPictureBookTopState extends State<PokemonPictureBookTop> {
 
-  String _imageUrl1 = "";
-  String _imageUrl2 = "";
-  String _imageUrl3 = "";
+  CallPokeApi callPokeApi = CallPokeApi();
+
+  // ChoiceChip(ゲームの世代)の要素数
+  late int _choiceChipCount;
+
+  // 世代データ取得APIリスト
+  List<String> _generationApiUrlList = [];
+
+  // 選択している世代のポケモンidリスト
+  // key:世代数 value:ポケモンidリスト
+  Map<String, List<Map>> _selectedGenePokemonIdListMap = {};
+
+  // listの要素数は表示数と1ずれるので注意
+  // 選択したChoiceChipの値
+  int? _selectedChoiceChipValue;
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
     // ここに非同期処理を入れると初期画面でステートを適応できる
+
+    getGenerationApiUrl();
+
   }
 
-  /// ポケモン画像取得
-  Future<void> callAPI() async {
-    var imageUrl1 = await CallPokeApi().getPokemon(id: "1");
-    var imageUrl2 = await CallPokeApi().getPokemon(id: "4");
-    var imageUrl3 = await CallPokeApi().getPokemon(id: "7");
+  Future<void> getGenerationApiUrl() async {
 
-    // 非同期処理が完了する前にウィジェットが破棄された場合（例: ユーザーが画面を移動した場合）、
-    // setStateが呼び出されるとエラーが発生するため、Widgetの存在確認をする
-    if(mounted){
-      // 上記の非同期が完了してから
-      setState(() {
-        _imageUrl1 = imageUrl1;
-        _imageUrl2 = imageUrl2;
-        _imageUrl3 = imageUrl3;
-      });
+    // 全世代ごとのAPI呼び出しURLを取得
+    var allGeneResponseJson  = await callPokeApi.getALLGenerationApiUrl();
+    print("===");
+    print(allGeneResponseJson);
+
+    // 世代数をセット
+    _choiceChipCount = allGeneResponseJson["count"];
+
+    // 世代APIをステートにセット
+    // Listの要素数を世代数として管理
+    for (Map urlMap in allGeneResponseJson["results"]) {
+      _generationApiUrlList.add(urlMap["url"]);
     }
+    print("===");
+    print(_generationApiUrlList);
+
   }
 
-  void clearUrl(){
-    setState(() {
-      _imageUrl1 = "";
-      _imageUrl2 = "";
-      _imageUrl3 = "";
-    });
+  Future<Map> getInitGenePokemonData() async {
+    // 世代１のポケモン画像を初期データとして読み込む
+    var allGene1 = await callPokeApi.getPokemonFromGeneration("1");
+    List<int> tmpResultList = [];
+    for (Map map in allGene1["pokemon_species"]) {
+      // "/"で区切って最後のidを取得
+      List<String> tmpList = map["url"].split("/");
+      tmpResultList.add(int.parse(tmpList[tmpList.length - 2]));
+    }
+    tmpResultList.sort();
+
+    // 世代マップとポケモンの紐付け
+    List<Map> tmpList = [];
+    for(int i in tmpResultList) {
+      tmpList.add(await callPokeApi.getPokemon(id: "${i}"));
+    }
+    print("===");
+    print("${tmpList[0]["id"]} :${tmpList[0]["name"]}");
+    _selectedGenePokemonIdListMap["1"] = tmpList;
+
+    return _selectedGenePokemonIdListMap;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if(_imageUrl1.isNotEmpty)
-                Image.network(_imageUrl1),
-              if(_imageUrl2.isNotEmpty)
-                Image.network(_imageUrl2),
-              if(_imageUrl3.isNotEmpty)
-                Image.network(_imageUrl3),
-            ],
+      appBar: AppBar(
+        title: const Text(
+          "ポケモン図鑑",
+          style: TextStyle(
+            color: Colors.white,
           ),
-
-          ElevatedButton(
-            onPressed: callAPI,
-            child: Text("get"),
-          ),
-          ElevatedButton(
-            onPressed: clearUrl,
-            child: Text("clear"),
-          ),
-        ],
         ),
       ),
+      body: FutureBuilder(
+        future: getInitGenePokemonData(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
 
+          if(snapshot.hasData){
+            Map<String, List<Map>> resultMap = snapshot.data;
+
+            return GridView.count(
+              crossAxisCount: 3,
+              crossAxisSpacing: 20.0,
+              mainAxisSpacing: 10.0,
+              children: [
+                for(Map tmpMap in resultMap["1"]!)
+                  Container(
+                    child: Image.network(tmpMap["sprites"]["front_default"]),
+                  ),
+              ],
+            );
+          } else {
+            return Center(
+              child: SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+            );
+
+          }
+        },
+
+      ),
     );
   }
 }
